fccf9cd8af4ef68b8429becf3f329c9c94133e4a
Krz <jrrobertokrz@gmail.com>
2024-05-15 01:18:05 -0300

    Adicionado long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+from pathlib import Path
+
+
+# read the contents of README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
 
 setup(
     name='PlacaFipy',
-    version='1.0.0',
+    version="1.0.3",
     packages=['placafipy'],
     install_requires=[
         'beautifulsoup4==4.12.3',
@@ -12,6 +19,8 @@ setup(
     author='Antônio Roberto Júnior',
     author_email='pix@jrkrz.com',
     description='Consultas FIPE pela placa do veículo.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/juniorkrz/placafipy',
     license='MIT',
     keywords='placa fipe veiculo'
